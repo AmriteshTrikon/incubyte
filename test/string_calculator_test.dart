03e81a2608af0aca73877d305f1afcfb52404aa3
Amritesh Khare <amritesh@trikon.io>
2025-02-17 23:55:36 +0530

    chnages according to the assignment

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -12,5 +12,37 @@ void main() {
     test('should return 0 for empty string', () {
       expect(calculator.add(''), equals(0));
     });
+
+    test('should return number for single number', () {
+      expect(calculator.add('1'), equals(1));
+    });
+
+    test('should return sum for two comma separated numbers', () {
+      expect(calculator.add('1,2'), equals(3));
+    });
+
+    test('should handle new lines between numbers', () {
+      expect(calculator.add('1\n2,3'), equals(6));
+    });
+
+    test('should support different delimiters', () {
+      expect(calculator.add('//;\n1;2'), equals(3));
+    });
+
+    test('should throw exception for negative numbers', () {
+      expect(
+        () => calculator.add('-1,2'),
+        throwsA(predicate(
+            (e) => e.toString() == 'negative numbers not allowed: -1')),
+      );
+    });
+
+    test('should show all negative numbers in exception', () {
+      expect(
+        () => calculator.add('-1,-2,3'),
+        throwsA(predicate(
+            (e) => e.toString() == 'negative numbers not allowed: -1,-2')),
+      );
+    });
   });
 }
