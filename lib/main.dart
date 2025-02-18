a23d73e45643a53e240ce4b26836e9d82bb0b300
Amritesh Khare <amritesh@trikon.io>
2025-02-18 13:16:29 +0530

    feat: implement empty string case (GREEN)

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,8 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) return 0;
-
-    String delimiter = ',';
-    String numbersToProcess = numbers;
-
-    if (numbers.startsWith('//')) {
-      delimiter = numbers[2];
-      numbersToProcess = numbers.substring(4);
+    if (numbers.isEmpty) {
+      return 0;
     }
-
-    List<String> numbersList =
-        numbersToProcess.replaceAll('\n', delimiter).split(delimiter);
-
-    List<int> negativeNumbers = [];
-    int sum = 0;
-
-    for (String number in numbersList) {
-      int n = int.parse(number);
-      if (n < 0) {
-        negativeNumbers.add(n);
-      }
-      sum += n;
-    }
-
-    if (negativeNumbers.isNotEmpty) {
-      throw Exception(
-          'negative numbers not allowed: ${negativeNumbers.join(',')}');
-    }
-
-    return sum;
+    return 0;
   }
 }
