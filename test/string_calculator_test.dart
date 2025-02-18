27f6576128f68030ef351880349bf741cfdbc605
Amritesh Khare <amritesh@trikon.io>
2025-02-18 13:20:57 +0530

    test: should return number for single number

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -12,5 +12,10 @@ void main() {
     test('should return 0 for empty string', () {
       expect(calculator.add(''), equals(0));
     });
+
+    test('should return number for single number', () {
+      expect(calculator.add('1'), equals(1));
+      expect(calculator.add('2'), equals(2));
+    });
   });
 }
